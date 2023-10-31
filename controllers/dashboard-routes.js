58e75b8490eaf825f8c12c0cd418f5770f711137
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-10-30 19:55:28 -0500

    created single post route and handlebar

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,10 +1,24 @@
 const router = require('express').Router();
 const auth = require('../utils/auth');
+const {User, Post} = require('../models');
 
-router.get('/dashboard', auth, (req, res) => {
-    res.render('dashboard', {
-        isLogged: req.session.isLogged
-    })
+router.get('/dashboard', auth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            include: [{
+                model: Post,
+            }]
+        })
+        const user = userData.get({plain: true})
+        console.log(user)
+        res.render('dashboard', {
+            isLogged: req.session.isLogged,
+            user
+        })
+    } catch (error) {
+        
+    }
+   
 })
 
 router.get('/dashboard/new', auth, (req, res) => {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,5 +36,32 @@ router.get('/signup', (req, res) => {
     res.render('signup')
 })
 
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const dbData = await Post.findByPk(req.params.id, {
+            attributes: ['id', 'title', 'content', 'created_at'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        const post = dbData.get({
+            plain: true
+        })
+        res.render('singlepost', {
+            isLogged: req.session.isLogged,
+            post
+        })
+    
+    } catch (error) {
+        res.render('home', {
+            isLogged: req.session.isLogged,
+            posts: []
+        })
+    }
+
+})
 
 module.exports = router;
