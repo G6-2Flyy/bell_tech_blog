e3fd1d68f283953a68d5c3c26b3b20ded83691fe
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-11-08 17:45:06 -0600

    added auto logout function

--- public/js/logout.js
+++ public/js/logout.js
@@ -20,3 +20,26 @@ async function logout () {
 }
 
 document.querySelector('#logout').addEventListener('click', logout)
+
+let timerId;
+let currentTimer = 0;
+
+function startIdleTime() {
+  currentTimer++;
+  if (currentTimer > 15) {
+    logout();
+  }
+}
+
+function resetTimer() {
+  clearInterval(timerId);
+  currentTimer = 0;
+  timerId = setInterval(startIdleTime, 60000);
+}
+
+window.onload = resetTimer;
+window.onmousemove = resetTimer;
+window.onmousedown = resetTimer;
+window.ontouchstart = resetTimer;
+window.onclick = resetTimer;
+window.onkeydown = resetTimer;
--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,20 @@
 function showSuccessMessage(msg) {
- const messageEl = document.querySelector('.message')
- const html = `<p class="success">${msg}</p>`
- messageEl.innerHTML = html
- setTimeout(()=> {
-    messageEl.innerHTML = ""
- }, 3000)
-} 
+  const messageEl = document.querySelector(".message");
+  const html = `<p class="success">${msg}</p>`;
+  messageEl.innerHTML = html;
+  setTimeout(() => {
+    messageEl.innerHTML = "";
+  }, 3000);
+}
 
 function showErrorMessage(msg) {
-    const messageEl = document.querySelector('.message')
-    const html = `<p class="error">${msg}</p>`
-    messageEl.innerHTML = html
-    setTimeout(()=> {
-       messageEl.innerHTML = ""
-    }, 3000)
-   } 
+  const messageEl = document.querySelector(".message");
+  const html = `<p class="error">${msg}</p>`;
+  messageEl.innerHTML = html;
+  setTimeout(() => {
+    messageEl.innerHTML = "";
+  }, 3000);
+}
+
+
+
